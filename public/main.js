425e9f53eacc97f731a6c5e62a03af1bd0c33ccb
cbrugman <102334290+cbrugman@users.noreply.github.com>
2023-04-30 11:03:16 -0400

    updates

--- public/main.js
+++ public/main.js
@@ -3,11 +3,14 @@ const weatherForm = document.querySelector('#weather-form')
 const cityInput = document.querySelector('#city-input')
 
 // Fetch weather data from API
-const fetchWeather = async (city) => {
+const detectBirds = async (city) => {
   const url = `/api?q=${city}`
-
+  
   const res = await fetch(url)
   const data = await res.json()
+
+  console.log(data.species)
+  console.log(data.detections)
 
   if (data.cod === '404') {
     alert('City not found')
@@ -20,8 +23,8 @@ const fetchWeather = async (city) => {
   }
 
   const displayData = {
-    city: data.name,
-    temp: kelvinToFahrenheit(data.main.temp),
+    species: data.species,
+    detections: data.detections,
   }
 
   addWeatherToDOM(displayData)
@@ -30,15 +33,15 @@ const fetchWeather = async (city) => {
 // Add display data to DOM
 const addWeatherToDOM = (data) => {
   weatherDisplay.innerHTML = `
-    <h1>Weather in ${data.city}</h1>
-    <h2>${data.temp} &deg;F</h2>
+    <h1>${data.species} species in</h1>
+    <h2>${data.detections} detections.</h2>
   `
   cityInput.value = ''
 }
 
 // Convert Kelvin to Fahrenheit
 const kelvinToFahrenheit = (temp) => {
-  return Math.ceil(((temp - 273.15) * 9) / 5 + 32)
+  //return Math.ceil(((temp - 273.15) * 9) / 5 + 32)
 }
 
 // Event listener for form submission
@@ -53,4 +56,4 @@ weatherForm.addEventListener('submit', (e) => {
 })
 
 // Initial fetch
-fetchWeather('Miami')
+detectBirds('stats')
