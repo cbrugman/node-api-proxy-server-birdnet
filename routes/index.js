cf3597cc9b00d3028e4b61cc88f06efb40ce1f4d
cbrugman <102334290+cbrugman@users.noreply.github.com>
2023-04-30 11:03:31 -0400

    updates

--- routes/index.js
+++ routes/index.js
@@ -18,13 +18,18 @@ router.get('/', cache('2 minutes'), async (req, res, next) => {
       [API_KEY_NAME]: API_KEY_VALUE,
       ...url.parse(req.url, true).query,
     })
-
-    const apiRes = await needle('get', `${API_BASE_URL}?${params}`)
+    
+    
+    apicommand = params.get('q')
+    const birdweatherURL = `${API_BASE_URL}${API_KEY_VALUE}/${apicommand}`
+    
+    const apiRes = await needle('get', birdweatherURL)
     const data = apiRes.body
+    
 
     // Log the request to the public API
     if (process.env.NODE_ENV !== 'production') {
-      console.log(`REQUEST: ${API_BASE_URL}?${params}`)
+      console.log(`REQUEST: ${birdweatherURL}`)
     }
 
     res.status(200).json(data)
